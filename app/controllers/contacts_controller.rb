ce156ed9e497ba70bee78374754056a87dad8e40
Alcy Torres <mxalcytorres@gmail.com>
2024-05-14 13:55:56 -0400

    live class updates from Brian

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,13 +6,7 @@ class ContactsController < ApplicationController
   
   def first
     # show data from database to user
-    contact = Contact.first
-    render json: {
-      id: contact.id,
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+    @contact = Contact.find_by(id: 2)
+    render template: "contacts/show" 
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,21 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Example:
-#
+
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+# contact = Contact.new(first_name: "John", last_name: "Smith", email: "johnsmith@gmail.com", phone_number: 617246864) 
+# contact = Contact.find_by(first_name: 'John')
+# contact.destroy
+
+# contact = Contact.new(first_name: "Michael", last_name: "Stark", email: "michaelstark@gmail.com", phone_number: 5083576412) 
+# contact = Contact.find_by(id: 6)
+# contact.destroy
+
+# contact = Contact.new(first_name: "Steven", last_name: "Rojas", email: "stevenrojas@gmail.com", phone_number: 4127862141) 
+# contact = Contact.find_by(id: 7)
+# contact.destroy
