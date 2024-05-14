e35775237ab51170672d1eb510018ef722de2e76
Alcy Torres <mxalcytorres@gmail.com>
2024-05-14 11:57:33 -0400

    render json for 'all contacts'

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,18 @@
 class ContactsController < ApplicationController
-  def hello
-    render json: {message: "Hello this is a test."}
+  def all
+    @contacts = Contact.all
+    render template: "contacts/index"
+  end
+  
+  def first
+    # show data from database to user
+    contact = Contact.first
+    render json: {
+      id: contact.id,
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.phone_number
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/practice-route" => "contacts#hello"
+  get "/show-contacts" => "contacts#first"
+  get "/all-contacts" => "contacts#all"
 
 end
